6cc63723a9badf562181ee51a3d1fd5587041430
umarbashirr <umarbashir93@outlook.com>
2025-01-04 12:34:24 +0530

    separate app logic to different file to keep index file clean

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, { Express } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Routes Import Files
+import authRoutes from "./routes/auth.routes";
+import propertiesRoutes from "./routes/properties.routes";
+
+// Initialize enviornment variables
+
+const app: Express = express();
+
+//  Middlewares
+app.use(morgan("combined"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/v1/pmsserver/auth", authRoutes);
+app.use("/api/v1/pmsserver/properties", propertiesRoutes);
+
+// Health Check Route
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript Node Express!");
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,9 @@
 require("dotenv").config();
 
-import express, { Express } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import helmet from "helmet";
-import morgan from "morgan";
-
-// Custom Imports
+import app from "./app";
 import connectToDB from "./config/db";
 
-// Routes Import Files
-import authRoutes from "./routes/auth.routes";
-import propertiesRoutes from "./routes/properties.routes";
-
-// Initialize enviornment variables
-
-const app: Express = express();
 const port = process.env.PORT || 5000;
-
-//  Middlewares
-app.use(morgan("combined"));
-app.use(helmet());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use("/api/v1/pmsserver/auth", authRoutes);
-app.use("/api/v1/pmsserver/properties", propertiesRoutes);
-
-// Health Check Route
-app.get("/", (req, res) => {
-  res.send("Hello, TypeScript Node Express!");
-});
 
 connectToDB()
   .then(() => {
