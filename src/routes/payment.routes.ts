e80c265ddfaf14b7e5ed0ebf99792424c5502efb
umarbashirr <umarbashir93@outlook.com>
2025-01-09 17:36:10 +0530

    payment routes file updated removed unnecessary imports

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,21 +1,12 @@
 import { RequestHandler, Router } from "express";
 import {
-  ASSIGN_ROOM_TO_LICENSE,
-  CANCEL_RESERVATION,
-  CHECK_IN_LICENSE,
-  CHECK_OUT_LICENSE,
-  CREATE_RESERVATION,
-  GET_RESERVATION_BY_ID,
-  UNASSIGN_ROOM_FROM_LICENSE,
-} from "../controllers/reservation.controller";
-import { CustomRequest } from "../interfaces/custom-request.interface";
-import verifyAccessToken from "../middlewares/verify-token.middleware";
-import { VerifyUserRole } from "../middlewares/verify-user-role.middleware";
-import {
   ADD_PAYMENT,
   CANCEL_PAYMENT,
   UPDATE_PAYMENT,
 } from "../controllers/payment.controller";
+import { CustomRequest } from "../interfaces/custom-request.interface";
+import verifyAccessToken from "../middlewares/verify-token.middleware";
+import { VerifyUserRole } from "../middlewares/verify-user-role.middleware";
 
 const router = Router({ mergeParams: true });
 
